fe62d4042434a0d3b93b7ab72dbfd9417f886ba9
WebOfPies <70017511+webofpies@users.noreply.github.com>
2025-01-15 13:37:09 +0400

    refactor: popup

--- frontend/src/components/Popup/PopupData.jsx
+++ frontend/src/components/Popup/PopupData.jsx
@@ -1,10 +1,9 @@
 // lute\templates\read\termpopup.html
 
 import { Group, Pill, PillGroup, Text, Tooltip } from "@mantine/core";
-import PopupDataSection from "./PopupDataSection";
 import classes from "./PopupData.module.css";
 
-export default function PopupData({ data }) {
+function PopupData({ data }) {
   return (
     data && (
       <div className={classes.container}>
@@ -39,17 +38,49 @@ export default function PopupData({ data }) {
           />
         )}
 
-        {data.parents.length > 0 && <PopupDataSection data={data.parents} />}
+        {data.parents.length > 0 && <Section data={data.parents} />}
 
         {data.components.length > 0 && (
           <>
             <Text component="p" mt="sm" fs="italic">
               Components
             </Text>
-            <PopupDataSection data={data.components} />
+            <Section data={data.components} />
           </>
         )}
       </div>
     )
   );
 }
+
+function Section({ data }) {
+  return data.map((d, index) => (
+    <div key={index} className={classes.section}>
+      <Group gap={5} wrap="nowrap">
+        <span
+          className={classes.term}
+          dangerouslySetInnerHTML={{
+            __html: d.text,
+          }}
+        />
+        {d.pronunciation && <em>({d.pronunciation})</em>}
+        {d.tags.length > 0 && (
+          <PillGroup gap={4}>
+            {d.tags.map((tag) => (
+              <Pill key={tag}>{tag}</Pill>
+            ))}
+          </PillGroup>
+        )}
+      </Group>
+      {d.translation && (
+        <span
+          dangerouslySetInnerHTML={{
+            __html: d.translation,
+          }}
+        />
+      )}
+    </div>
+  ));
+}
+
+export default PopupData;
